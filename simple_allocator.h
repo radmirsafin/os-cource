ef25e87517374f2064f78fb20ffad23bbf14dcde
radmirsafin <dev-rsafin@yandex.ru>
2020-11-17 23:15:34 +0300

    simple allocator library
    (cherry picked from commit 1a0c8f79923384ecc02cec86aa0c2aa0d553ae76)

---
+++ simple_allocator.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLE_ALLOCATOR_H
+#define SIMPLE_ALLOCATOR_H
+
+void print_memory_map();
+int get_free_size();
+void mysetup(void *buf, int size);
+void *myalloc(int size);
+void myfree(void *p);
+
+#endif //SIMPLE_ALLOCATOR_H
--- test_simple_allocator.cpp
+++ test_simple_allocator.cpp
@@ -1,111 +1,12 @@
+#include "simple_allocator.h"
+
 #include <iostream>
 #include <cstdio>
 #include <malloc.h>
 #include <cmath>
 #include <ctime>
 #include <cstdlib>
-
-
-int metadata_size = 0;
-struct metadata {
-    struct metadata *next;
-    struct metadata *prev;
-    bool is_free;
-    int size;
-};
-
-struct metadata *memory_blocks = NULL;
-
-void print_memory_map() {
-    struct metadata *current = memory_blocks;
-    printf("\n=> Memory map\n");
-    while (current != NULL) {
-        printf("metadata { address=%p, next=%p, prev=%p, size=%d, is_free=%d, end=%p}\n",
-               current, current->next, current->prev, current->size, current->is_free,
-               reinterpret_cast<unsigned char *>(current) + metadata_size + current->size);
-        current = current->next;
-    }
-}
-
-void mysetup(void *buf, int size) {
-    printf("Initialize memory: size=%d, start_address=%p \n", size, buf);
-
-    metadata_size = sizeof(struct metadata);
-
-    struct metadata *initial = static_cast<metadata *>(buf);
-    initial->next = NULL;
-    initial->prev = NULL;
-    initial->size = size - metadata_size;
-    initial->is_free = true;
-
-    memory_blocks = static_cast<metadata *>(buf);
-}
-
-void *myalloc(int size) {
-    struct metadata *current = memory_blocks;
-    while (current != NULL) {
-        if (current->is_free) {
-            if (current->size == size) {
-                current->is_free = false;
-                return reinterpret_cast<unsigned char *>(current) + metadata_size;
-            } else if (current->size > metadata_size + size) {
-                current->size -= metadata_size + size + 1;
-
-                unsigned char *alloc_block_addr =
-                        reinterpret_cast<unsigned char *>(current) + metadata_size + current->size + 1;
-                struct metadata *new_block = reinterpret_cast<metadata *>(alloc_block_addr);
-                new_block->next = current->next;
-                new_block->prev = current;
-                new_block->size = size;
-                new_block->is_free = false;
-
-                if (current->next != NULL) {
-                    current->next->prev = new_block;
-                }
-                current->next = new_block;
-
-                return alloc_block_addr + metadata_size;
-            }
-        }
-        current = current->next;
-    }
-    return NULL;
-}
-
-struct metadata *merge_blocks(struct metadata *first, struct metadata *second) {
-    first->size += metadata_size + second->size + 1;
-    if (second->next != NULL) {
-        second->next->prev = first;
-    }
-    first->next = second->next;
-    return first;
-}
-
-void myfree(void *p) {
-    unsigned char *free_block_addr = reinterpret_cast<unsigned char *>(p) - metadata_size;
-    struct metadata *free_block = reinterpret_cast<metadata *>(free_block_addr);
-    free_block->is_free = true;
-
-    while (free_block->next && free_block->next->is_free) {
-        free_block = merge_blocks(free_block, free_block->next);
-    }
-
-    while (free_block->prev && free_block->prev->is_free) {
-        free_block = merge_blocks(free_block->prev, free_block);
-    }
-}
-
-int get_free_size() {
-    int free = 0;
-    struct metadata *current = memory_blocks;
-    while (current != NULL) {
-        if (current->is_free) {
-            free += current->size;
-        }
-        current = current->next;
-    }
-    return free;
-}
+#include <functional>
 
 int max_size_test(bool progress=false) {
     printf("\n========== TEST: max_size_test ==========\n");
@@ -142,7 +43,7 @@ int max_size_test(bool progress=false) {
         }
     }
 
-    if (max_size_percent >= 0.9) {
+    if (max_size_percent >= 90) {
         printf("[PASSED] Max size: %f %%\n", max_size_percent);
         return 0;
     } else {
@@ -181,7 +82,7 @@ int effective_size_test(bool progress=false) {
         }
     }
 
-    if (effective_size_percent >= 0.9) {
+    if (effective_size_percent >= 30) {
         printf("[PASSED] Effective size: %f %%\n", effective_size_percent);
         return 0;
     } else {
@@ -240,8 +141,9 @@ int allocation_test(bool progress=false) {
 }
 
 int main() {
-    max_size_test();
-    effective_size_test();
-    allocation_test();
-    return 0;
+    int failed_tests = 0;
+    failed_tests += max_size_test();
+    failed_tests += effective_size_test();
+    failed_tests += allocation_test();
+    return failed_tests;
 }

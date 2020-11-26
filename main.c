349745d9f8e75d9498747cc934005d180752d3b7
radmirsafin <dev-rsafin@yandex.ru>
2020-11-27 00:49:39 +0300

    base functions
    (cherry picked from commit 83745cb8fb1c7f90fffb145a0a0b948de1f2cba4)

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "math.h"
 #include "malloc.h"
+#include "stdint.h"
 
 /**
  * Эти две функции вы должны использовать для аллокации
@@ -17,14 +18,30 @@
  **/
 void *alloc_slab(int order) {
     int size = 4096 * (int) pow(2, order);
-    return memalign(size, size);
+    void * allocated_ptr = memalign(size, size);
+    printf("SLAB memory allocated: %p - %p\n", allocated_ptr, allocated_ptr + size);
+    return allocated_ptr;
 }
 
 /**
  * Освобождает участок ранее аллоцированный с помощью
  * функции alloc_slab.
  **/
-void free_slab(void *slab);
+void free_slab(void *slab) {
+    free(slab);
+}
+
+
+struct object {
+    struct object* next;
+};
+
+struct slab {
+    int free_count;
+    struct object* free_obj_head;
+    struct slab* prev;
+    struct slab* next;
+};
 
 
 /**
@@ -34,13 +51,13 @@ void free_slab(void *slab);
  * сохранить в этой структуре.
  **/
 struct cache {
-    /* список пустых SLAB-ов для поддержки cache_shrink */
-    /* список частично занятых SLAB-ов */
-    /* список заполненых SLAB-ов */
+    struct slab* empty_head; /* список пустых SLAB-ов для поддержки cache_shrink */
+    struct slab* used_head;  /* список частично занятых SLAB-ов */
+    struct slab* full_head;  /* список заполненых SLAB-ов */
 
-    int object_size; /* размер аллоцируемого объекта */
-    int slab_order; /* используемый размер SLAB-а */
-    int slab_objects; /* количество объектов в одном SLAB-е */
+    int object_size;         /* размер аллоцируемого объекта */
+    int slab_order;          /* используемый размер SLAB-а */
+    int slab_objects;        /* количество объектов в одном SLAB-е */
 };
 
 
@@ -54,9 +71,95 @@ struct cache {
  **/
 void cache_setup(struct cache *cache, int object_size)
 {
-    /* Реализуйте эту функцию. */
+    cache->slab_order = 10;
+    cache->object_size = object_size;
+    int bytes_available = (int) (4096 * pow(2, cache->slab_order) - sizeof(struct slab));
+    cache->slab_objects = bytes_available / object_size;
 }
 
+struct slab* create_slab(int slab_order, int slab_objects, int object_size) {
+    void *allocated_ptr = alloc_slab(slab_order);
+    struct slab* new = (struct slab*) allocated_ptr;
+    new->free_count = slab_objects;
+
+    struct object* prev_obj = allocated_ptr + sizeof(struct slab) + 1;
+    new->free_obj_head = prev_obj;
+
+    for (int i = 0; i < slab_objects - 1; i++) {
+        struct object* next_obj = (void*) prev_obj + object_size + 1;
+        prev_obj->next = next_obj;
+        prev_obj = next_obj;
+    }
+    return new;
+}
+
+void remove_slab_from_cache(struct cache* cache, struct slab* slab) {
+    if (slab == cache->empty_head) {
+        cache->empty_head = slab->next;
+    } else if (slab == cache->used_head) {
+        cache->used_head = slab->next;
+    } else if (slab == cache->full_head) {
+        cache->full_head = slab->next;
+    }
+
+    if (slab->prev) {
+        slab->prev->next = slab->next;
+    }
+    if (slab->next) {
+        slab->next->prev = slab->prev;
+    }
+}
+
+void add_slab_to_cache(struct cache* cache, struct slab* slab) {
+    if (slab->free_count == 0) {
+        slab->next = cache->full_head;
+        if (cache->full_head) {
+            cache->full_head->prev = slab;
+        }
+        cache->full_head = slab;
+    } else if (slab->free_count != cache->slab_objects) {
+        slab->next = cache->used_head;
+        if (cache->used_head) {
+            cache->used_head->prev = slab;
+        }
+        cache->used_head = slab;
+    } else {
+        slab->next = cache->empty_head;
+        if (cache->empty_head) {
+            cache->empty_head->prev = slab;
+        }
+        cache->empty_head = slab;
+    }
+}
+
+/**
+ * Функция аллокации памяти из кеширующего аллокатора.
+ * Должна возвращать указатель на участок памяти размера
+ * как минимум object_size байт (см cache_setup).
+ * Гарантируется, что cache указывает на корректный
+ * инициализированный аллокатор.
+ **/
+void *cache_alloc(struct cache *cache) {
+    struct slab *source = NULL;
+    if (cache->used_head != NULL) {
+        source = cache->used_head;
+        remove_slab_from_cache(cache, source);
+    } else if (cache->empty_head != NULL) {
+        source = cache->empty_head;
+        remove_slab_from_cache(cache, source);
+    } else {
+        source = create_slab(cache->slab_order,
+                             cache->slab_objects,
+                             cache->object_size);
+    }
+
+    source->free_count -= 1;
+    void *allocated_ptr = source->free_obj_head;
+    source->free_obj_head = source->free_obj_head->next;
+
+    add_slab_to_cache(cache, source);
+    return allocated_ptr;
+}
 
 /**
  * Функция освобождения будет вызвана когда работа с
@@ -70,20 +173,6 @@ void cache_release(struct cache *cache)
     /* Реализуйте эту функцию. */
 }
 
-
-/**
- * Функция аллокации памяти из кеширующего аллокатора.
- * Должна возвращать указатель на участок памяти размера
- * как минимум object_size байт (см cache_setup).
- * Гарантируется, что cache указывает на корректный
- * инициализированный аллокатор.
- **/
-void *cache_alloc(struct cache *cache)
-{
-    /* Реализуйте эту функцию. */
-}
-
-
 /**
  * Функция освобождения памяти назад в кеширующий аллокатор.
  * Гарантируется, что ptr - указатель ранее возвращенный из
@@ -91,7 +180,18 @@ void *cache_alloc(struct cache *cache)
  **/
 void cache_free(struct cache *cache, void *ptr)
 {
-    /* Реализуйте эту функцию. */
+    uintptr_t xor_mask = 4096 * pow(2, cache->slab_order) - 1;
+    xor_mask = ~xor_mask;
+    struct slab* source = (struct slab *) ((uintptr_t) ptr & xor_mask);
+
+    remove_slab_from_cache(cache, source);
+
+    source->free_count += 1;
+    struct object* free_obj = ptr;
+    free_obj->next = source->free_obj_head;
+    source->free_obj_head = free_obj;
+
+    add_slab_to_cache(cache, source);
 }
 
 
@@ -109,6 +209,18 @@ void cache_shrink(struct cache *cache)
 
 
 int main() {
-    void *allocated = alloc_slab(0);
-    printf("Allocated: %p", allocated);
+    printf("Size of slab: %lu\n", sizeof(struct slab));
+    printf("Size of cache: %lu\n", sizeof(struct cache));
+
+    struct cache* cache = malloc(sizeof(struct cache));
+    cache_setup(cache, 32768);
+
+    for (int i = 0; i < 126; i++) {
+        cache_alloc(cache);
+    }
+    void * allocated_ptr = cache_alloc(cache);
+    printf("Allocated: %p\n", allocated_ptr);
+
+    cache_free(cache, allocated_ptr);
+    printf("");
 }
